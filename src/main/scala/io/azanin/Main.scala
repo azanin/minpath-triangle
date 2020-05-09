1d52cd2ec58c419a2385880eb733d48451b8f3b7
azanin <ale.zanin90@gmail.com>
2020-05-09 12:04:20 +0200

    add program logic

--- src/main/scala/io/azanin/Main.scala
+++ src/main/scala/io/azanin/Main.scala
@@ -1,13 +1,19 @@
 package io.azanin
 
-import cats.Order
+import cats.{ Order, Show }
 import cats.data.Ior
-import cats.effect.{ ExitCode, IO, IOApp }
+import cats.effect.{ ExitCode, IO, IOApp, Resource }
 import cats.implicits._
+
+import scala.io.Source
+import scala.util.Try
 
 object Main extends IOApp {
 
   implicit val orderPath: Order[Path] = (x: Path, y: Path) => x.total.compare(y.total)
+
+  implicit val nodeShow: Show[Node] = (n: Node) => n.toString
+  implicit val showPath: Show[Path] = (t: Path) => s"Minimal path is: ${t.nodes.map(_.show).mkString("+")} = ${t.total}"
 
   case class Node(value: Int) extends AnyVal
   case class Path(total: Int, nodes: List[Node])
@@ -25,6 +31,19 @@ object Main extends IOApp {
         }
     }
 
+  def parseTriangle(rows: List[String]): Try[List[List[Node]]] =
+    rows.traverse(_.split(" ").toList.traverse(n => Try(Node(n.toInt))))
+
   override def run(args: List[String]): IO[ExitCode] =
-    IO.delay(println("Hello world")).as(ExitCode.Success)
+    Resource
+      .fromAutoCloseable(IO.delay(Source.fromFile("data.txt")))
+      .use { bs =>
+        for {
+          lines    <- IO.delay(bs.getLines().toList)
+          triangle <- IO.fromTry(parseTriangle(lines))
+          path     = minPathTriangle(triangle).headOption
+          repr     = path.fold("No Path for the specified Triangle")(_.show)
+        } yield println(repr)
+      }
+      .as(ExitCode.Success)
 }
--- src/test/scala/io/azanin/Test.scala
+++ src/test/scala/io/azanin/Test.scala
@@ -4,10 +4,10 @@ import io.azanin.Main.{ Node, Path }
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-import scala.util.Random
+import scala.util.{ Random, Success }
 
 class Test extends AnyFunSuite with Matchers {
-  test("Correctness of the alghoritm") {
+  test("Correctness of the algorithm") {
 
     val input = List(
       List(Node(7)),
@@ -31,6 +31,25 @@ class Test extends AnyFunSuite with Matchers {
     val actual = Main.minPathTriangle(input)
 
     actual.nonEmpty.shouldBe(true)
+  }
+
+  test("parse triangle") {
+
+    val input          = List("1", "2 3", "4 5 6")
+    val expectedResult = Success(List(List(Node(1)), List(Node(2), Node(3)), List(Node(4), Node(5), Node(6))))
+
+    val actual = Main.parseTriangle(input)
+
+    actual.shouldBe(expectedResult)
+  }
+
+  test("error in parsing triangle") {
+
+    val input = List("1", "notANumber 3", "4 5 6")
+
+    val actual = Main.parseTriangle(input)
+
+    actual.isFailure.shouldBe(true)
   }
 
 }
