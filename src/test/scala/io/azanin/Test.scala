125f16eeb2fe04a984196d7bc0859cc9b01ad141
azanin <ale.zanin90@gmail.com>
2020-05-09 11:30:19 +0200

    add unit case to check if 500 rows triangle is supported

--- src/test/scala/io/azanin/Test.scala
+++ src/test/scala/io/azanin/Test.scala
@@ -4,6 +4,8 @@ import io.azanin.Main.{ Node, Path }
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
+import scala.util.Random
+
 class Test extends AnyFunSuite with Matchers {
   test("Correctness of the alghoritm") {
 
@@ -19,4 +21,16 @@ class Test extends AnyFunSuite with Matchers {
 
     actual.shouldBe(expectedResult)
   }
+
+  test("500 rows triangle") {
+
+    val input = (for {
+      rows <- 1 to 500
+    } yield List.fill(rows)(Node(Random.nextInt(20)))).toList
+
+    val actual = Main.minPathTriangle(input)
+
+    actual.nonEmpty.shouldBe(true)
+  }
+
 }
