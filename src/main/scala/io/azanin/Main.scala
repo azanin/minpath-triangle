f4c40ebf9f64f94ab8639e5cf10045cf9d5a053b
azanin <ale.zanin90@gmail.com>
2020-05-09 10:38:07 +0200

    Add a unit test case to pass

--- src/main/scala/io/azanin/Main.scala
+++ src/main/scala/io/azanin/Main.scala
@@ -4,6 +4,11 @@ import cats.effect.{ ExitCode, IO, IOApp }
 
 object Main extends IOApp {
 
+  case class Node(value: Int) extends AnyVal
+  case class Path(total: Int, nodes: List[Node])
+
+  def minPathTriangle(input: List[List[Int]]): List[Path] = ???
+
   override def run(args: List[String]): IO[ExitCode] =
     IO.delay(println("Hello world")).as(ExitCode.Success)
 }
--- src/test/scala/io/azanin/Test.scala
+++ src/test/scala/io/azanin/Test.scala
@@ -1,10 +1,18 @@
 package io.azanin
 
-import cats.effect.IO
+import io.azanin.Main.{ Node, Path }
+import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-class Test extends AsyncFunIOSpec with Matchers {
+class Test extends AnyFunSuite with Matchers {
   test("The test should be true") {
-    IO(true).asserting(b => b shouldBe true)
+
+    val input = List(List(7), List(6, 3), List(3, 8, 5), List(11, 2, 10, 9))
+
+    val actual = Main.minPathTriangle(input)
+
+    val expectedResult = List(Path(18, List(Node(7), Node(6), Node(3), Node(2))))
+
+    actual.shouldBe(expectedResult)
   }
 }
