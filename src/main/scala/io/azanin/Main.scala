4ae1772e4a0477fe904fd564f338d8dea10c7dfb
azanin <ale.zanin90@gmail.com>
2020-05-09 11:25:38 +0200

    implement correct alghoritm

--- src/main/scala/io/azanin/Main.scala
+++ src/main/scala/io/azanin/Main.scala
@@ -1,13 +1,29 @@
 package io.azanin
 
+import cats.Order
+import cats.data.Ior
 import cats.effect.{ ExitCode, IO, IOApp }
+import cats.implicits._
 
 object Main extends IOApp {
+
+  implicit val orderPath: Order[Path] = (x: Path, y: Path) => x.total.compare(y.total)
 
   case class Node(value: Int) extends AnyVal
   case class Path(total: Int, nodes: List[Node])
 
-  def minPathTriangle(input: List[List[Int]]): List[Path] = ???
+  def minPathTriangle(input: List[List[Node]]): List[Path] =
+    input.foldRight(List(): List[Path]) {
+      case (row, paths) =>
+        val couples = paths.zip(paths.drop(1)) //Can't use tail since is throwing exception on empty list
+        row.alignWith(couples) {
+          case Ior.Both(node, (left, right)) =>
+            val chosenPath = left.min(right)
+            Path(chosenPath.total + node.value, node +: chosenPath.nodes)
+          case Ior.Left(node) => Path(node.value, List(node))
+          case _              => Path(0, Nil)
+        }
+    }
 
   override def run(args: List[String]): IO[ExitCode] =
     IO.delay(println("Hello world")).as(ExitCode.Success)
--- src/test/scala/io/azanin/Test.scala
+++ src/test/scala/io/azanin/Test.scala
@@ -5,12 +5,16 @@ import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 class Test extends AnyFunSuite with Matchers {
-  test("The test should be true") {
+  test("Correctness of the alghoritm") {
 
-    val input = List(List(7), List(6, 3), List(3, 8, 5), List(11, 2, 10, 9))
+    val input = List(
+      List(Node(7)),
+      List(Node(6), Node(3)),
+      List(Node(3), Node(8), Node(5)),
+      List(Node(11), Node(2), Node(10), Node(9))
+    )
 
-    val actual = Main.minPathTriangle(input)
-
+    val actual         = Main.minPathTriangle(input)
     val expectedResult = List(Path(18, List(Node(7), Node(6), Node(3), Node(2))))
 
     actual.shouldBe(expectedResult)
